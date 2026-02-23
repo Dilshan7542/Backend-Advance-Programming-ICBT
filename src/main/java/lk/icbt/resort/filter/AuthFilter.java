de44509f0b3a139a3bd4b96634af65f70e84bf94
dilshan_k <dilshan_k@epiclanka.net>
2026-02-23 14:01:28 +0530

    add auth filter

---
+++ src/main/java/lk/icbt/resort/filter/AuthFilter.java
@@ -0,0 +1,35 @@
+package lk.icbt.resort.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.icbt.resort.util.AppConstants;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/dashboard",
+        "/rooms",
+        "/customers/*",
+        "/reservations/*",
+        "/billing",
+        "/billing/*",
+        "/help"
+})
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        Object user = (req.getSession(false) != null) ? req.getSession(false).getAttribute(AppConstants.SESSION_USER) : null;
+        if (user == null) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
